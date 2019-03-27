c8f5dceaf98738594660a61ade6e5919fe18d61f
JEverett <jonathan@jonathaneverettcreative.com>
2019-03-27 18:23:03 -0500

    Correctly console logging the first artist of each result.

--- liri.js
+++ liri.js
@@ -13,9 +13,12 @@ var arg = process.argv[3];
 var runUserCommand = function() {
     if (userCommand === "spotify-this-song") {
         console.log("Testing this function");
-        spotify.search({type: 'track', query: arg})
+        spotify.search({type: 'track', query: arg, limit: 5})
         .then(function(response) {
-            console.log(JSON.stringify(response, null, 2));
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                console.log("Artist/Band: " + response.tracks.items[i].artists[0].name);
+            }
+            // console.log(JSON.stringify(response, null, 2));
         })
         .catch(function(err) {
             console.log(err);
