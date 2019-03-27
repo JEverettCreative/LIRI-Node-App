8cad2f6eef3899afad53f884746905d35f4a3ca8
JEverett <jonathan@jonathaneverettcreative.com>
2019-03-26 22:32:14 -0500

    Fixed function that takes user input and calls Spotify using the node package. Currently, it calls ALL data about the given song, with no limit.

--- liri.js
+++ liri.js
@@ -2,10 +2,26 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var Spotify = require("node-spotify-api");
+
 var spotify = new Spotify(keys.spotify);
 
 var userCommand = process.argv[2];
 
+var arg = process.argv[3];
+
 var runUserCommand = function() {
-    
+    if (userCommand === "spotify-this-song") {
+        console.log("Testing this function");
+        spotify.search({type: 'track', query: arg})
+        .then(function(response) {
+            console.log(JSON.stringify(response, null, 2));
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    }
 }
+
+
+runUserCommand();
