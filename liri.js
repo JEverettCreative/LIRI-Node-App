e81b011ca15591d84aa27f1a0dd90473a9465d11
JEverett <jonathan@jonathaneverettcreative.com>
2019-03-28 12:21:55 -0500

    Created if/else statement that evaluates if a song parameter has been input. If no input, defaults to Ace of Base 'The Sign' through a spotify.request.

--- liri.js
+++ liri.js
@@ -12,21 +12,39 @@ var arg = process.argv[3];
 
 var runUserCommand = function() {
     if (userCommand === "spotify-this-song") {
-        console.log("Testing this function");
-        spotify.search({type: 'track', query: arg, limit: 5})
-        .then(function(response) {
-            for (var i = 0; i < response.tracks.items.length; i++) {
-                console.log("Result " + (i + 1));
-                console.log("Artist/Band: " + response.tracks.items[i].artists[0].name);
-                console.log("Song Title: " + response.tracks.items[i].name);
-                console.log("Preview Link: " + response.tracks.items[i].preview_url);
+        if (arg) {
+            spotify.search({type: 'track', query: arg, limit: 5})
+            .then(function(response) {
+                for (var i = 0; i < response.tracks.items.length; i++) {
+                    console.log("Result " + (i + 1));
+                    console.log("Artist/Band: " + response.tracks.items[i].artists[0].name);
+                    console.log("Song Title: " + response.tracks.items[i].name);
+                    console.log("Preview Link: " + response.tracks.items[i].preview_url);
+                    console.log("Album Title: " + response.tracks.items[i].album.name);
+                    console.log("\n-----------------\n");
 
-            }
-            // console.log(JSON.stringify(response, null, 2));
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
+                }
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        }
+        else if (!arg) {
+            console.log("\n");
+            // Default on blank to "The Sign"
+            spotify.request("https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE")
+            .then(function(response) {  
+                    
+                    console.log("Artist/Band: " + response.artists[0].name);
+                    console.log("Song Title: " + response.name);
+                    console.log("Preview Link: " + response.preview_url);
+                    console.log("Album Title: " + response.album.name);
+                    console.log("\n-----------------\n");
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        }
     }
 }
 
